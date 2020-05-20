b1fd2fba65731c7b538745027402612c37ee88e3
paperbenni <paperbenni@gmail.com>
2020-05-20 23:39:07 +0200

    more dependencies

--- depend.sh
+++ depend.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "installing dependencies to build instantos packages"
-sudo pacman -S --needed wmctrl xdotool go ninja meson check
+sudo pacman -S --needed \
+    wmctrl \
+    xdotool \
+    go \
+    ninja \
+    meson \
+    check \
+    rxvt-unicode \
+    libnotify
