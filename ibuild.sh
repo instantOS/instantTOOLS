9b2bfd8e7f5d4dc900894071f3effc95e41dafce
paperbenni <paperbenni@gmail.com>
2020-09-12 20:01:46 +0200

    format

--- ibuild.sh
+++ ibuild.sh
@@ -6,10 +6,8 @@
 
 # this script is the main wrapper
 
-if ! [ -e /usr/share/instanttools ]
-then
-    if [ -e "$PREFIX/share/instanttools" ]
-    then
+if ! [ -e /usr/share/instanttools ]; then
+    if [ -e "$PREFIX/share/instanttools" ]; then
         TOOLS="$PREFIX/share/instanttools"
     else
         echo "ibuild not installed correctly"
@@ -19,19 +17,15 @@ else
     TOOLS="/usr/share/instanttools"
 fi
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     SCRIPT="$(ls "$TOOLS/" | grep -o '[^/]*$' | grep -o '^[^.]*' | fzf | head -1)"
 else
-    if [ -e "$TOOLS/$1.sh" ]
-    then
+    if [ -e "$TOOLS/$1.sh" ]; then
         SCRIPT="$1"
     else
         SLIST="$(ls "$TOOLS/" | grep "^$1")"
-        if [ -n "$SLIST" ]
-        then
-            if wc -l <<< "$SLIST" | grep -o '^1$'
-            then
+        if [ -n "$SLIST" ]; then
+            if wc -l <<<"$SLIST" | grep -o '^1$'; then
                 SCRIPT="$(echo "$SLIST" | grep -o '[^/]*$' | grep -o '^[^.]*')"
             else
                 SCRIPT="$(ls "$TOOLS/" | grep "^$1" | grep -o '[^/]*$' | grep -o '^[^.]*' | fzf | head -1)"
@@ -46,4 +40,3 @@ fi
 
 [ -z "$SCRIPT" ] && exit
 "$TOOLS/$SCRIPT.sh" "$@"
-
