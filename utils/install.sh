31464ab549cccc0d42b2d3f111f5b14dee4b5955
paperbenni <paperbenni@gmail.com>
2020-09-19 14:20:25 +0200

    improve install UI

--- utils/install.sh
+++ utils/install.sh
@@ -3,7 +3,33 @@
 # doc: build and install a single instantOS package
 
 if [ -z "$1" ]; then
-    echo "usage: ibuild install packagename"
+    if ! [ -e ~/workspace/extra ]
+    then
+        echo "building cache"
+        mkdir -p ~/workspace
+        git clone --depth=1 https://github.com/instantOS/extra
+    fi
+
+    cd ~/workspace/extra || exit 1
+    PACKAGELIST=""
+    for i in ./*
+    do
+        if [ -e "$i"/PKGBUILD ]
+        then
+            PACKAGENAME="$(grep -o '[^./]*' <<< "$i")"
+            PACKAGELIST="$PACKAGELIST
+$PACKAGENAME"
+        fi
+    done
+    PACKAGELIST="$(grep '..'<<< "$PACKAGELIST")"
+    TARGET="$(fzf <<< "$PACKAGELIST")"
+    if [ -z "$TARGET" ] || ! [ -e ~/workspace/extra/"$TARGET"/PKGBUILD ]
+    then
+        echo "usage: ibuild install packagename"
+    else
+        ibuild install "$TARGET"
+    fi
+
     exit
 fi
 
@@ -20,6 +46,7 @@ fi
 
 if ! [ -e "$1" ]; then
     echo "package $1 not found"
+    echo "run ibuild install to get a list of available packages"
     exit
 fi
 
