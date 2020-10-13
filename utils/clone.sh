d4f764a03694344fb2c6d3e9817bc0b332039fad
paperbenni <paperbenni@gmail.com>
2020-10-13 21:14:10 +0200

    git clone fzf

--- utils/clone.sh
+++ utils/clone.sh
@@ -2,40 +2,37 @@
 
 # doc: clone github stuff
 
-if [ -z "$1" ]
-then
-    echo "usage: i c repo"
-    exit
-fi
-
-
-if [ "$1" -eq "$1" ] &> /dev/null && [ -n "$2" ]
-then
+if [ "$1" -eq "$1" ] &>/dev/null; then
     GITARGS="--depth=$1"
     shift 1
 fi
 
-if ! grep -q '/' <<< "$1"
-then
-    export GIT_ASKPASS="ibuild"
-    if git ls-remote -h "https://github.com/instantOS/$1" 
-    then
-        GITREPO="https://github.com/instantOS/$1" 
-    else
-        GITREPO="https://github.com/instantOS/instant$1" 
+if [ -z "$1" ]; then
+    GITREPO="$(
+        curl -s "https://api.github.com/users/$GHUSER/repos?per_page=100" | grep -E -o 'git@[^"]*' | grep -o ':.*' | grep -E -o '[^:]*' | fzf
+    )"
+    if [ -z "$REPO" ]; then
+        echo "usage: i c repo"
+        exit
     fi
-elif ! grep -q '//' <<< "$1"
-then
-   GITREPO="https://github.com/$1" 
 else
-   GITREPO="$1" 
-fi
+    if ! grep -q '/' <<<"$1"; then
+        export GIT_ASKPASS="ibuild"
+        if git ls-remote -h "https://github.com/instantOS/$1"; then
+            GITREPO="https://github.com/instantOS/$1"
+        else
+            GITREPO="https://github.com/instantOS/instant$1"
+        fi
+    elif ! grep -q '//' <<<"$1"; then
+        GITREPO="https://github.com/$1"
+    else
+        GITREPO="$1"
+    fi
 
-if [ -n "$2" ]
-then
-    GITDEST="$2"
+    if [ -n "$2" ]; then
+        GITDEST="$2"
+    fi
 fi
 
 echo "cloning git repo $GITREPO"
 eval "$(echo git clone "$GITARGS" "$GITREPO" "$GITDEST" | sed 's/  */ /g')" || exit 1
-
