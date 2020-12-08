14126946948f553954eaf8cb8191cc7655c77b69
paperbenni <paperbenni@gmail.com>
2020-12-08 23:19:13 +0100

    add github.sh

---
+++ utils/github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /usr/share/paperbash/import.sh
+pb git
+
+{
+    ghrepos instantOS | grep -o '/.*' | sed 's/\.git$//g' | grep -o '[^/]*' >~/.cache/instantos/repos2 &&
+        cp ~/.cache/instantos/repos2 ~/.cache/instantos/repos
+} &
+
+[ -e ~/.cache/instantos/repos ] || sleep 10
+[ -e ~/.cache/instantos/repos ] || exit 1
+
+REPOCHOICE="$(
+    imenu -l <~/.cache/instantos/repos
+)"
+
+[ -z "$REPOCHOICE" ] && exit 1
+instantutils open browser "https://github.com/instantOS/$REPOCHOICE"
--- utils/instantarchtest.sh
+++ utils/instantarchtest.sh
@@ -2,6 +2,10 @@
 
 # doc: build and install a single instantOS package
 
-curl -Ls git.io/instantarch > instantarch.sh
+TESTBRANCH="$(git ls-remote https://github.com/instantos/instantarch | grep 'refs/heads' | grep -o 'refs/.*' | sed 's/^refs\/.*\///g' | imenu -l)"
+[ -z "$TESTBRANCH" ] && exit 1
+
+curl -s https://raw.githubusercontent.com/instantOS/instantARCH/"$TESTBRANCH"/archinstall.sh >instantarch.sh
+
 chmod +x ./instantarch.sh
 sudo ./instantarch.sh test
