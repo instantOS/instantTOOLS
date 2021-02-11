020f8afe62db2902051e888905e75cc50a381219
paperbenni <paperbenni@gmail.com>
2021-02-11 18:56:03 +0100

    init localbackup

---
+++ utils/localbackup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# doc: create a local backup of all instantOS repos
+
+if iconf backuplocation; then
+    BTARGET="$(iconf backuplocation)"
+else
+    BTARGET="$HOME/instantosbackup"
+fi
+
+if ! [ -e "$BTARGET" ]; then
+    echo "creating new backup target"
+    mkdir "$BTARGET"
+    cd "$BTARGET" || exit 1
+    echo 'This is a static backup of the instantOS source code, please do not touch' >README.txt
+else
+    cd "$BTARGET" || exit 1
+fi
+
+REPOLIST="$(
+    curl -s "https://api.github.com/users/instantOS/repos?per_page=100" | grep -o 'git@github.com:instantOS.*' |
+        sed 's/",$//g' | sed 's/^.*://g' | sed 's/.git$//g' | grep -o '[^/]*$'
+)"
+
+REPOCOUNT="$(echo "$REPOLIST" | wc -l)"
+
+if ! [ "$REPOCOUNT" -gt 20 ]; then
+    echo "there's something wrong with github, couldn't get all repos"
+    exit 1
+fi
+
+echo "Backing up repos:
+$REPOLIST"
+
+while read -r p; do
+    echo "processing repo $p"
+    if [ -e "./$p" ]; then
+        echo "updating repo $p"
+        cd "$p" || exit 1
+        git fetch --all || exit 1
+        git pull --all || exit 1
+        cd .. || exit 1
+    else
+        git clone https://github.com/instantOS/"$p" || exit 1
+    fi
+done <<<"$REPOLIST"
+
+echo "finished instantOS local backup of $REPOCOUNT repos"
